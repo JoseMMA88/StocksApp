90af218256c29fad5dd3f3f261abbb96526a37bf
Jose Manuel Malagón Alba <contact@josemalagon.es>
2021-12-10 14:13:23 +0100

    Initial Commit

---
+++ StocksApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksApp
+//
+//  Created by Jose Manuel Malagón Alba on 10/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Jose Manuel Malagón Alba on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
